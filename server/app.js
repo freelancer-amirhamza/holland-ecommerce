ab2e7c755ff26e4f44a312214d9d3e655441121a
Amir <amirhamza27940@gmail.com>
2024-08-13 16:59:57 +0600

    update

--- server/app.js
+++ server/app.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+
+
+
+
+
+app.use(cors());
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+
 
 app.get("/", (req, res)=>{
     res.send("<h1>Welcome to Home server</h1>")
